b26a9a166f14a43748aebc594b4b9c94e2d376e7
Paul Akhere <paulakhere48@gmail.com>
2025-01-02 13:53:56 +0100

    animation

--- src/component/items/Items.jsx
+++ src/component/items/Items.jsx
@@ -3,27 +3,31 @@ import { Link } from "react-router-dom";
 const Items = ({ image, price, title, button, product, id }) => {
   return (
     <div className="item shadow rounded p-3 py-4 ">
-      <img
-        className="mb-3 item-image"
-        src={image}
-        onClick={window.scrollTo(0, 0)}
-        alt=""
-      />
-      <p className="d-flex item-heading justify-content-center my-2">
-        {title} ...
-      </p>
-      <div className="item-prices">
-        <p className="item-price-new d-flex text-danger justify-content-center my-2">
-          ${price}
-        </p>
+      <div className="item-image-div">
+        <img
+          className="mb-3 item-image"
+          src={image}
+          onClick={window.scrollTo(0, 0)}
+          alt=""
+        />
       </div>
-      <div className="d-flex justify-content-center my-2">
-        <Link
-          to={`/products/${product}`}
-          className="btn btn-outline-dark item-button btn-sm"
-        >
-          {button}
-        </Link>
+      <div className="item-description-div">
+        <p className="d-flex item-heading justify-content-center my-2">
+          {title} ...
+        </p>
+        <div className="item-prices">
+          <p className="item-price-new d-flex text-danger justify-content-center my-2">
+            ${price}
+          </p>
+        </div>
+        <div className="d-flex justify-content-center my-2">
+          <Link
+            to={`/products/${product}`}
+            className="btn btn-outline-dark item-button btn-sm"
+          >
+            {button}
+          </Link>
+        </div>
       </div>
     </div>
   );
